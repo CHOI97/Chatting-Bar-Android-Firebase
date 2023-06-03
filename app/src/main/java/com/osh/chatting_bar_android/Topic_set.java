6b2cb26f73df0db872284075ca455e893da62a92
bae-suho <sugh6019@naver.com>
2023-06-04 03:38:34 +0900

    [FEAT] 회원가입 관심주제 설정 리사이클러뷰 추가, 구글 로그인 기능 삭제

--- app/src/main/java/com/osh/chatting_bar_android/Topic_set.java
+++ app/src/main/java/com/osh/chatting_bar_android/Topic_set.java
@@ -1,20 +1,27 @@
 package com.osh.chatting_bar_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Topic_set extends AppCompatActivity {
 
+    private EditTagRecyclerViewAdapter EditTagRecyclerViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_set);
 
         InitBtn();
+        InitTagList();
     }
 
     protected void InitBtn()
@@ -40,5 +47,31 @@ public class Topic_set extends AppCompatActivity {
                 finish();
             }
         });
+        //다음에 하기
+        Button next = findViewById(R.id.Topic_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                LoginActivity loginActivity = (LoginActivity)LoginActivity.loginActivity;
+                loginActivity.finish();
+
+                finish();
+            }
+        });
+    }
+    protected void InitTagList(){
+        RecyclerView recyclerView = findViewById(R.id.topicSet_recyclerView);
+
+        EditTagRecyclerViewAdapter = new EditTagRecyclerViewAdapter(this, getTagList());
+        recyclerView.setAdapter(EditTagRecyclerViewAdapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+    }
+
+    private List<String> getTagList() {
+        return Arrays.asList("home_deco", "volunteering", "백계환", "배종찬", "신초은","배수호", "오시현", "백계환", "배종찬", "신초은","배수호", "오시현", "백계환", "배종찬", "신초은","배수호", "오시현","백계환", "배종찬", "신초은","배수호", "오시현", "백계환", "배종찬", "신초은");
     }
 }
